c0eac41760ec9cfbe09d9bdf7d097d9c0186e36d
KatyaEspinosa03 <katyamiranda03@hotmail.com>
2023-10-01 17:48:32 -0600

    Connected all buttons to the view controller and added tags to the numbers on the attributes inspector.

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var operationLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func numbers(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func operators(_ sender: UIButton) {
+    }
+    
+    @IBAction func otherButtons(_ sender: UIButton) {
+    }
+    
+    @IBAction func equalButton(_ sender: UIButton) {
+    }
+    
+    
 }
 
